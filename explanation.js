4eea2401159eb1269b85ffd160a1355bc8044d63
the-dekisch <68425941+the-dekisch@users.noreply.github.com>
2020-07-17 16:40:11 +0100

    Added a bunch of JS code, added the CSS syle sheet and html basic command

---
+++ explanation.js
@@ -0,0 +1,47 @@
+// Explanations Variables
+var favColor = 'blue';
+var myFavColors = ['blue', 'red', 'yellow'];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName: 'Eva',
+	lastName: 'Deckers',
+	favColors: ['blue', 'red', 'yellow'],
+	yearsAlive: 28,
+	isFemale: true
+};
+
+
+// Explanation Functions
+
+var whatIsMyFavColor = function () {
+	return 'blue';
+}
+
+
+var area = function (width, height) {
+	return width * height;
+}
+
+var fullname = function(firstname, secondname) {
+	return firstname + ' ' +secondname;
+}
+
+
+// Explanations IF Statements
+
+if (true) {
+	// do something
+	alert('male');
+}
+
+if (1 == 2) {
+	alert ('male')
+}
+
+var number1 = 10
+var number2 = 100
+
+if (number2 > number1) {
+	alert('male')
+}
